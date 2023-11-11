732949934cead0d97721a3823ab8b9899a0b0808
Khanh Sy <sykhanh1996@gmail.com>
2023-11-11 22:59:34 +0700

    axios and react query

--- AIWorkoutCoachReact/src/main.tsx
+++ AIWorkoutCoachReact/src/main.tsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      // Provide the client to your App
+      <QueryClientProvider client={queryClient}>
+        <App />
+        {/* The rest of your application */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
