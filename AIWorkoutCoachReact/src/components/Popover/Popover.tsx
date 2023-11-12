4e58d8f0bb775e759d9d3e30c207568521f221b4
Khanh Sy <sykhanh1996@gmail.com>
2023-11-12 18:52:56 +0700

    popver cart

--- AIWorkoutCoachReact/src/components/Popover/Popover.tsx
+++ AIWorkoutCoachReact/src/components/Popover/Popover.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { FloatingPortal, useFloating, arrow, FloatingArrow, offset } from '@floating-ui/react'
+import { FloatingPortal, useFloating, arrow, FloatingArrow, offset, type Placement } from '@floating-ui/react'
 import { ElementType, useId, useRef, useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 
@@ -9,8 +9,16 @@ interface Props {
   className?: string
   as?: ElementType
   initialOpen?: boolean
+  placement?: Placement
 }
-export default function Popover({ children, renderPopover, className, as: Element = 'div', initialOpen }: Props) {
+export default function Popover({
+  children,
+  renderPopover,
+  className,
+  as: Element = 'div',
+  initialOpen,
+  placement = 'bottom-end'
+}: Props) {
   const [open, setOpen] = useState(initialOpen || false)
   const arrowRef = useRef(null)
 
@@ -20,7 +28,8 @@ export default function Popover({ children, renderPopover, className, as: Elemen
         element: arrowRef
       }),
       offset(6)
-    ]
+    ],
+    placement: placement
   })
   const id = useId()
   const showPopover = () => {
