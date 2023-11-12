fd034a318273ecb643c98a224a0ed4d1b269447d
Khanh Sy <sykhanh1996@gmail.com>
2023-11-12 19:08:27 +0700

    setup protected route and rejected route

--- AIWorkoutCoachReact/src/useRouteElements.tsx
+++ AIWorkoutCoachReact/src/useRouteElements.tsx
@@ -1,40 +1,66 @@
-import { Outlet, useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import RegisterLayout from './layouts/RegisterLayout'
 import Register from './pages/Register'
 import path from './constants/path'
 import { Suspense, lazy } from 'react'
 import MainLayout from './layouts/MainLayout'
 import ProductList from './pages/ProductList'
+import Profile from './pages/Profile'
 
+const isAuthenticated = false
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
 function RejectedRoute() {
-  return <Outlet />
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
 }
 const Login = lazy(() => import('./pages/Login'))
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
-      path: '/',
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
         </MainLayout>
-      )
-    },
-    {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
-    },
-    {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
       )
     }
   ])
