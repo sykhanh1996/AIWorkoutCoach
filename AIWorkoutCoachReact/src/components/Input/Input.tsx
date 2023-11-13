f9f7b76eedd93e1dae1cfa094dac3d0003019e1b
Khanh Sy <sykhanh1996@gmail.com>
2023-11-13 11:51:58 +0700

    UI for AsideFilter

--- AIWorkoutCoachReact/src/components/Input/Input.tsx
+++ AIWorkoutCoachReact/src/components/Input/Input.tsx
@@ -1,29 +1,41 @@
+import { InputHTMLAttributes } from 'react'
 import type { UseFormRegister, RegisterOptions } from 'react-hook-form'
 
-interface Props {
-  type: React.HTMLInputTypeAttribute
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  placeholder?: string
-  name: string
-  register: UseFormRegister<any>
+  register?: UseFormRegister<any>
   rules?: RegisterOptions
   label?: React.ReactNode
-  autoComplete?: string
+  classNameInput?: string
+  classNameError?: string
 }
 
-export default function Input({ type, errorMessage, placeholder, name, register, rules, label, autoComplete }: Props) {
+export default function Input({
+  type,
+  errorMessage,
+  placeholder,
+  className,
+  name,
+  register,
+  rules,
+  label,
+  autoComplete,
+  classNameInput = 'form-input bg-gray-50 w-full border border-gray-300 rounded-lg text-gray-900 text-sm p-2.5 block focus:ring-blue-600 focus:border-blue-500 focus:outline-none focus:border-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+  classNameError = 'mt-1 px-2 text-red-600 text-sm min-h-[1rem]'
+}: Props) {
+  const registerResult = register && name ? register(name, rules) : {}
+
   return (
-    <div>
+    <div className={className}>
       {label}
       <input
         type={type}
         placeholder={placeholder}
-        className='form-input bg-gray-50 w-full border border-gray-300 rounded-lg text-gray-900 text-sm p-2.5 block focus:ring-blue-600 focus:border-blue-500
-                    focus:outline-none focus:border-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        className={classNameInput}
         autoComplete={autoComplete}
-        {...register(name, rules)}
+        {...registerResult}
       ></input>
-      <div className='mt-1 px-2 text-red-600 text-sm min-h-[1rem]'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
